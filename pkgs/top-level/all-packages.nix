b45390c171dc3bd9e430d503325450ae3d8308d1
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-29 16:43:36 +0200

    Revert "collada-dom: drop"
    This reverts commit e69e90619309c91d027b7b0aeaee7a103b670005.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20230,6 +20230,8 @@ with pkgs;
 
   cointop = callPackage ../applications/misc/cointop { };
 
+  collada-dom = callPackage ../development/libraries/collada-dom { };
+
   coloquinte = callPackage ../development/libraries/science/electronics/coloquinte { };
 
   cog = callPackage ../development/web/cog { };
