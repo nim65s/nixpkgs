421ef58aadc37fae98ca4a7bb1db39f4a1cd1a88
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-29 16:56:33 +0200

    gepetto-viewer-unwrapped: init at 5.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4869,6 +4869,8 @@ self: super: with self; {
 
   georss-wa-dfes-client = callPackage ../development/python-modules/georss-wa-dfes-client { };
 
+  gepetto-viewer-unwrapped = callPackage ../development/python-modules/gepetto-viewer-unwrapped { };
+
   gerbonara = callPackage ../development/python-modules/gerbonara { };
 
   getjump = callPackage ../development/python-modules/getjump { };
