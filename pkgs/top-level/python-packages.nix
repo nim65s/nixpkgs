8c69c0cf038c88d80281a10bbdb5dc2229149c32
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-03 13:20:57 +0200

    hpp-environments: init at 6.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5963,6 +5963,11 @@ self: super: with self; {
     python3Packages = self;
   });
 
+  hpp-environments = toPythonModule (pkgs.hpp-environments.override {
+    pythonSupport = true;
+    python3Packages = self;
+  });
+
   hs-dbus-signature = callPackage ../development/python-modules/hs-dbus-signature { };
 
   hsaudiotag3k = callPackage ../development/python-modules/hsaudiotag3k { };
