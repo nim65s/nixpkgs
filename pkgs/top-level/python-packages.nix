79447c1e661a51c9f8763c40e84f227794196dbf
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-03 13:54:25 +0200

    ndcurves: init at 1.4.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8865,6 +8865,8 @@ self: super: with self; {
 
   natural = callPackage ../development/python-modules/natural { };
 
+  ndcurves = toPythonModule (pkgs.ndcurves.override { python3Packages = self; });
+
   nomadnet = callPackage ../development/python-modules/nomadnet { };
 
   noneprompt = callPackage ../development/python-modules/noneprompt {  };
