c479ed1e56ef0f277960dfd1b52b6dfaccc18d0c
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-03 14:00:38 +0200

    hpp-baxter: init at 5.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5610,6 +5610,8 @@ self: super: with self; {
 
   hpccm = callPackage ../development/python-modules/hpccm { };
 
+  hpp-baxter = pkgs.hpp-baxter.override { python3Packages = self; };
+
   hpp-bezier-com-traj = pkgs.hpp-bezier-com-traj.override { python3Packages = self; };
 
   hpp-centroidal-dynamics = pkgs.hpp-centroidal-dynamics.override { python3Packages = self; };
