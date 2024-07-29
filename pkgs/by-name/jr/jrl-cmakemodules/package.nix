aa897a4bcf3740c69d22e5245e6776eb84e8fac4
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-29 17:03:25 +0200

    jrl-cmakemodules: 0-unstable-2024-05-22 -> 0-unstable-2024-07-27

--- pkgs/by-name/jr/jrl-cmakemodules/package.nix
+++ pkgs/by-name/jr/jrl-cmakemodules/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation {
   pname = "jrl-cmakemodules";
-  version = "0-unstable-2024-05-22";
+  version = "0-unstable-2024-07-27";
 
   src = fetchFromGitHub {
     owner = "jrl-umi3218";
     repo = "jrl-cmakemodules";
-    rev = "dd3e4127f2b223496859fe05be02726e9d1a071b";
-    hash = "sha256-ySMfops0Py1U+50LEGwgIwlzPUq/miusTxK7TIpQQXs=";
+    rev = "5f9b31ac21ad8ece333b7bfae90bc8a4f60aa9c8";
+    hash = "sha256-ozewWBhkB0Tz4iAtxagus50Fohqm8A/CcXJfh61Fn0Y=";
   };
 
   nativeBuildInputs = [ cmake ];
