154222664526d6ac714ddd1a3854e22b68f8fd58
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-29 19:51:42 +0200

    hpp-tutorial: init at 6.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6085,6 +6085,8 @@ self: super: with self; {
     }
   );
 
+  hpp-tutorial = toPythonModule (pkgs.hpp-tutorial.override { python3Packages = self; });
+
   hpp-universal-robot = toPythonModule (
     pkgs.hpp-universal-robot.override {
       pythonSupport = true;
