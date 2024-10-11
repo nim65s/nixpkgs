2179446ec4baef377235d330265b49b97854efef
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-11 10:15:20 +0200

    open3d

--- pkgs/by-name/op/open3d/package.nix
+++ pkgs/by-name/op/open3d/package.nix
@@ -1,19 +1,31 @@
 {
-  lib,
-  stdenv,
-  cmake,
-  fetchFromGitHub,
-  python3,
   assimp,
-  nanoflann,
-  glfw,
-  jsoncpp,
-  git,
+  cmake,
   eigen,
+  fetchFromGitHub,
+  filament,
+  fmt,
+  git,
+  glew,
+  glfw,
+  gtest,
+  imgui,
+  jsoncpp,
+  lib,
+  liblzf,
+  libpng,
+  nanoflann,
+  python3Packages,
+  qhull,
+  stdenv,
+  tinygltf,
+  tinyobjloader,
   withExamples ? false,
   withCuda ? false,
+  withPython ? false,
   withGui ? true,
 }:
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "open3d";
   version = "0.18.0";
@@ -25,14 +37,41 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-VMykWYfWUzhG+Db1I/9D1GTKd3OzmSXvwzXwaZnu8uI=";
   };
 
+  nativeBuildInputs = [
+    cmake
+    git
+  ];
+  buildInputs = [
+    assimp
+    eigen
+    filament
+    fmt
+    glew
+    glfw
+    gtest
+    imgui
+    jsoncpp
+    libpng
+    liblzf
+    nanoflann
+    python3Packages.python
+    qhull
+    tinygltf
+    tinyobjloader
+  ];
+
   cmakeFlags = [
-    (lib.cmakeBool "BUILD_EXAMPLES" withExamples)
     (lib.cmakeBool "BUILD_CUDA_MODULE" withCuda)
+    (lib.cmakeBool "BUILD_EXAMPLES" withExamples)
+    (lib.cmakeBool "BUILD_PYTHON_MODULE" withPython)
     (lib.cmakeBool "BUILD_GUI" withGui)
     (lib.cmakeBool "BUILD_WEBRTC" withGui)
     (lib.cmakeBool "BUILD_JUPYTER_EXTENSION" withGui)
     "-DBUILD_SHARED_LIBS=ON"
     "-DBUILD_UNIT_TESTS=ON"
+    #"-DBUILD_ISPC_MODULE=OFF" # TODO: it really tries to fetch stuff
+    #"-DENABLE_HEADLESS_RENDERING=ON" it is either that or GUI
+    "-DUSE_SYSTEM_BLAS=ON"
     "-DUSE_SYSTEM_ASSIMP=ON"
     "-DUSE_SYSTEM_CURL=ON"
     "-DUSE_SYSTEM_CUTLASS=ON"
@@ -63,30 +102,17 @@ stdenv.mkDerivation (finalAttrs: {
     "-DUSE_SYSTEM_LIBREALSENSE=ON"
     "-DBUILD_TENSORFLOW_OPS=ON"
     "-DBUILD_PYTORCH_OPS=ON"
+    "-DBUILD_VTK_FROM_SOURCE=OFF"
+    "-DBUILD_FILAMENT_FROM_SOURCE=OFF"
+    "-DPREFER_OSX_HOMEBREW=OFF"
     # TODO
     "-DBUILD_ISPC_MODULE=OFF"
   ];
 
-  nativeBuildInputs = [
-    cmake
-    git
-  ];
-
-  buildInputs = [
-    python3
-    assimp
-    glfw
-    jsoncpp
-  ];
-
-  propagatedBuildInputs = [
-    eigen
-    nanoflann
-  ];
-
   meta = {
-    changelog = "https://github.com/isl-org/Open3D/blob/v${finalAttrs.version}/CHANGELOG.md";
-    description = "A Modern Library for 3D Data Processing";
+    description = "Modern Library for 3D Data Processing";
+    homepage = "https://github.com/isl-org/Open3D";
+    changelog = "https://github.com/isl-org/Open3D/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.nim65s ];
   };
