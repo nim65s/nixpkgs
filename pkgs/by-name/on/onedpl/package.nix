a9094b9af350fbc01292423759706ed5b8d56018
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-17 16:50:56 +0200

    onedpl: init at 2022.6.0

---
+++ pkgs/by-name/on/onedpl/package.nix
@@ -0,0 +1,32 @@
+{
+  cmake,
+  fetchFromGitHub,
+  lib,
+  stdenv,
+  tbb_2021_11,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "onedpl";
+  version = "2022.6.0";
+
+  src = fetchFromGitHub {
+    owner = "oneapi-src";
+    repo = "onedpl";
+    rev = "oneDPL-${finalAttrs.version}-rc1";
+    hash = "sha256-VdVdv+dpdhe10SB50AHiim5Glwjv0xCwxYoPetGXQi8=";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [
+    tbb_2021_11
+  ];
+
+  meta = {
+    description = "implementation of the oneAPI specification for the oneDPL component";
+    homepage = "https://github.com/oneapi-src/onedpl";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ nim65s ];
+    platforms = lib.platforms.unix;
+  };
+})
