f4486673c081eff119df3b73264a3b0150fb6e58
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-11-14 16:29:23 +0100

    mptcpd: init at 0.13

---
+++ nixos/modules/services/networking/mptcpd.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+
+  cfg = config.services.mptcpd;
+
+in
+
+{
+
+  options = {
+
+    services.mptcpd = {
+
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          Whether to enable the Multipath TCP path management daemon
+        '';
+      };
+
+      package = lib.mkPackageOption pkgs "mptcpd" { };
+
+    };
+
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = [ cfg.package ];
+
+    systemd.packages = [ cfg.package ];
+
+  };
+
+  meta.maintainers = with lib.maintainers; [ nim65s ];
+}
