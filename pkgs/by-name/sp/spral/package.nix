a3c481bc66c31687b5654c9da71be256bc196c3e
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-09 11:37:59 +0200

    spral: drop obsolete patch

--- pkgs/by-name/sp/spral/package.nix
+++ pkgs/by-name/sp/spral/package.nix
@@ -22,20 +22,13 @@ stdenv.mkDerivation rec {
     hash = "sha256-9QEcAOFB3CtGNqr8LoDaj2vP3KMONlUVooeXECtGsxc=";
   };
 
-  postPatch =
-    ''
-      # Skipped test: ssidst
-      # hwloc/linux: failed to find sysfs cpu topology directory, aborting linux discovery.
-      substituteInPlace tests/meson.build --replace-fail \
-        "subdir('ssids')" \
-        ""
-    ''
-    + lib.optionalString stdenv.hostPlatform.isDarwin ''
-      # Skipped test: lsmrt, segfault
-      substituteInPlace tests/meson.build --replace-fail \
-        "['lsmrt', files('lsmr.f90')]," \
-        ""
-    '';
+  postPatch = ''
+    # Skipped test: ssidst
+    # hwloc/linux: failed to find sysfs cpu topology directory, aborting linux discovery.
+    substituteInPlace tests/meson.build --replace-fail \
+      "subdir('ssids')" \
+      ""
+  '';
 
   nativeBuildInputs = [
     gfortran
