ffb2848afa0cc29d40bd3a989b52d33b3e6ba9a1
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-08-07 00:48:57 +0200

    metis: patch for CMake v4

--- pkgs/by-name/me/metis/package.nix
+++ pkgs/by-name/me/metis/package.nix
@@ -15,6 +15,14 @@ stdenv.mkDerivation rec {
     sha256 = "1cjxgh41r8k6j029yxs8msp3z6lcnpm16g5pvckk35kc7zhfpykn";
   };
 
+  # Fix for CMake v4
+  # ref. https://github.com/KarypisLab/METIS/commit/350931887dfc00c2e3cb7551c5abf30e0297126a
+  postPatch = ''
+    substituteInPlace CMakeLists.txt --replace-fail \
+      "cmake_minimum_required(VERSION 2.8)" \
+      "cmake_minimum_required(VERSION 3.10)"
+  '';
+
   cmakeFlags = [
     "-DGKLIB_PATH=../GKlib"
     # remove once updated past https://github.com/KarypisLab/METIS/commit/521a2c360dc21ace5c4feb6dc0b7992433e3cb0f
