b9f56057a8fff9966667057471008fa2d0ec8e0d
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-08-07 15:25:41 +0200

    qhull: patch for CMake v4

--- pkgs/by-name/qh/qhull/package.nix
+++ pkgs/by-name/qh/qhull/package.nix
@@ -17,6 +17,15 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-djUO3qzY8ch29AuhY3Bn1ajxWZ4/W70icWVrxWRAxRc=";
   };
 
+  # Fix build with CMake v4
+  # Ref. https://github.com/qhull/qhull/pull/158
+  # This was merged upstream and can be removed on next release
+  postPatch = ''
+    substituteInPlace CMakeLists.txt --replace-fail \
+      "cmake_minimum_required(VERSION 3.0)" \
+      "cmake_minimum_required(VERSION 3.5...4.0)"
+  '';
+
   nativeBuildInputs = [ cmake ] ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames;
 
   meta = with lib; {
