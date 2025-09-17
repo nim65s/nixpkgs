d7eea3f136c076b11c8e463687f3410f85472394
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-09-17 18:56:57 +0200

    nexus wip

--- pkgs/by-name/vc/vclab-nexus/package.nix
+++ pkgs/by-name/vc/vclab-nexus/package.nix
@@ -3,25 +3,35 @@
 
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
 
   # nativeBuildInputs
   cmake,
   libsForQt5,
 
   # buildInputs
+  corto,
+  glew,
   vcg,
 }:
 
-stdenv.mkDerivation rec {
-  pname = "vclab-nexus";
+stdenv.mkDerivation (finalAttrs: {
+  pname = "vclab-nexus"; # nexus is already taken
   version = "2025.05";
 
   src = fetchFromGitHub {
     owner = "cnr-isti-vclab";
     repo = "nexus";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-lC3nKQJwkixwGzPP/oS0J+WIFgYqky6NaXu9bX28+3I=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/nim65s/nexus/commit/e742f16.patch";
+      hash = "sha256-zoJuLp11DNpacIWTSCQst0NDGM9rJ2xeLLX2NaSLm7I=";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
@@ -29,6 +39,8 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    corto
+    glew
     libsForQt5.qtbase
     vcg
   ];
@@ -41,4 +53,4 @@ stdenv.mkDerivation rec {
     mainProgram = "nxsview";
     platforms = lib.platforms.unix ++ lib.platforms.windows;
   };
-}
+})
